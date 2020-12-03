eeb7606c0010167231f42f9958302c83713ea9cb
seventrio <dimakrikunov12@gmail.com>
2020-12-03 11:10:07 +0200

    start transition to hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,20 @@
-import React from "react";
-import {
-    BrowserRouter as Router,
-} from "react-router-dom";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { loadUsersFromStorage } from "../../actions/usersActions";
 import './App.scss'
 
 import Header from "../Header/Header";
 import Routes from "../Routes/Routes";
 
 const App = ({ addUser, users }) => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        console.log("App useEffect");
+        dispatch(loadUsersFromStorage())
+    }, [dispatch])
+
     return (
         <Router>
             <Header/>
